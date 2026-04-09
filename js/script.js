5bade9afa646f81a84c686a1dcb8bda1db4e97b1
admin12place <thierry@randovelo34.fr>
2026-04-09 11:22:58 +0200

    finalisation oscars et fleur footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+//rechargement de la page quand on click sur un lien => #
 document.querySelectorAll('.anchor-link').forEach(link => {
   link.addEventListener('click', () => {
     window.location.reload();
   });
 });
+
