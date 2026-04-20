2e16c9fb3c8bbcf1759014e40a04f36e806f1d17
admin12place <thierry@randovelo34.fr>
2026-04-20 18:25:06 +0200

    scsset ajustement landscape

--- template-parts/banner-menu.php
+++ template-parts/banner-menu.php
@@ -1,11 +1,16 @@
-<ul class="stretch-banner">
-    <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-        <img src="<?php echo get_theme_file_uri() . '/assets/logo-stretch-banner.png'; ?> " alt="logo Fleurs d'oranger & chats errants"/>
-    </a></li>
-    <li><a class="anchor-link" href="#story">Histoire</a></li>
-    <li><a class="anchor-link" href="#characters">Personnages</a></li>
-    <li><a class="anchor-link" href="#place">Lieu</a></li>
-    <li><a class="anchor-link" href="#studio">Studio Koukaki</a></li>
-    <div class="banner-footer"><a href=""<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"">STUDIO KOUKAKI</a></div>
-</ul>
+<div class="stretch-banner">
+    <div>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php echo get_theme_file_uri() . '/assets/logo-stretch-banner.png'; ?> " alt="logo Fleurs d'oranger & chats errants"/>
+        </a>
+    </div>
+
+    <ul class="menu-banner">
+        <li><a class="anchor-link" href="#story">Histoire</a></li>
+        <li><a class="anchor-link" href="#characters">Personnages</a></li>
+        <li><a class="anchor-link" href="#place">Lieu</a></li>
+        <li><a class="anchor-link" href="#studio">Studio Koukaki</a></li>
+        <div class="banner-footer"><a href=""<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"">STUDIO KOUKAKI</a></div>
+    </ul>
+</div>
 
